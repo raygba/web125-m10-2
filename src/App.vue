88cb565eda34690cb00f8adc2e3184e7543342e5
raygba <rkraiss@my.waketech.edu>
2025-11-01 15:09:12 -0400

    create quote generator

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1>Random Quote Generator</h1>
+
+    <!-- display the quote or a loading message -->
+    <p v-if="loading">Loading...</p>
+    <blockquote v-else>
+      <p class="quote">"{{ quote }}"</p>
+      <p class="author">&ndash; {{ author }}</p>
+    </blockquote>
+
+    <!-- button that triggers API call -->
+    <button @click="getQuote">Get New Quote</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      quote: '', // stores quote text
+      author: '',
+      loading: false // tracks loading status
+    }
+  },
+  methods: {
+    async getQuote() {
+      this.loading = true
+      try {
+        // send GET request to Quotable API
+        const response = await axios.get('https://quotes15.p.rapidapi.com/quotes/random/?language_code=en', {
+          headers: {
+            'x-rapid-api-host': 'quotes15.p.rapidapi.com',
+            'x-rapidapi-key': 'bf63059798msh9641885127d8bdap1cebc1jsnfaf887995c25'
+          }
+        })
+        //update the quote with the response data
+        this.quote = response.data.content;
+        this.author = response.data.originator.name;
+      } catch (error) {
+        console.error('Error fetching quote:', error)
+        this.quote = 'Oops! Something went wrong.'
+        this.author = ''
+      } finally {
+        this.loading = false
+      }
+    }
+  },
+  mounted() {
+    // load a random quote when the app first starts
+    this.getQuote()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  background-color: gainsboro;
+}
+
+.container {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Georgia, serif;
+  margin-top: 50px;
+}
+
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 16px;
+}
+
+.quote {
+  max-width: 50ch;
+  margin: auto auto;
+}
+
+.author {
+  font-style: italic;
+  color: #555;
+  margin-top: 1rem;
 }
 </style>
