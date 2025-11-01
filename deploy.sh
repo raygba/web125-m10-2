bc2d434203d76b2a8d2a64d704a6d2ea2cc796a4
raygba <rkraiss@my.waketech.edu>
2025-11-01 14:27:35 -0400

    new deployment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-#usr/bin/env sh
+#/usr/bin/env sh
+
 set -e
 npm run build
 cd dist
-# git init
 git add -A
 git commit -m "new deployment"
-git push -f g@github.com:raygba/web125-m10-2.git main:gh-pages
+git push -f git@github.com:raygba/web125-m10-2.git main:gh-pages
 cd -
