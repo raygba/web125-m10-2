de9c687b4f2953f5b74e79eff42a91bc330be4a1
raygba <rkraiss@my.waketech.edu>
2025-11-01 15:12:48 -0400

    created quote generator

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ npm run build
 cd dist
 git add -A
 git commit -m "new deployment"
-git push -f git@github.com:raygba/web125-m10-2.git main
+git push -f git@github.com:raygba/web125-m10-2.git main:gh-pages
 cd -
