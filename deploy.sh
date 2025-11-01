583f7ab45e97a12eaf65909a035bf797cf153bb8
raygba <rkraiss@my.waketech.edu>
2025-11-01 14:01:25 -0400

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#usr/bin/env sh
+set -e
+npm run build
+cd dist
+git init
+git add -A
+git commit -m 'new deployment'
+git push -f g@github.com:raygba/web125-m10-2.git main:gh-pages
+cd -
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  publicPath: '/web125-m10-2/',
+  transpileDependencies: true
+})
